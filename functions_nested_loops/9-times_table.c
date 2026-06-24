7a8f8113631767ed641102b7d12291aea54f3abe
Corbel99 <thomas.corbel35000@gmail.com>
2026-06-24 17:06:30 +0200

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,41 +4,41 @@
  */
 void times_table(void)
 {
-    int n;
-    int i;
-    int resultat;
+	int n;
+	int i;
+	int resultat;
 
-    for (n= 0; n < 10; n++)
-    {
-        for (i = 0; i <10; i++)
-        {
-            resultat = n * i;
+	for (n= 0; n < 10; n++)
+	{
+		for (i = 0; i <10; i++)
+		{
+			resultat = n * i;
 
-             if (i > 0)
-            {
-                 if (resultat >= 10)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                }
-                else
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-            }    
-            if (resultat >= 10)
-            {
-                _putchar(resultat / 10 + '0');
-                _putchar(resultat % 10 + '0');
-            }
-            else
-            {
-            _putchar(resultat + '0');
-            }
-        }
-        _putchar('\n');
-    }
+			 if (i > 0)
+			{
+				 if (resultat >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}    
+			if (resultat >= 10)
+			{
+				_putchar(resultat / 10 + '0');
+				_putchar(resultat % 10 + '0');
+			}
+			else
+			{
+			_putchar(resultat + '0');
+			}
+		}
+		_putchar('\n');
+	}
   
 }
