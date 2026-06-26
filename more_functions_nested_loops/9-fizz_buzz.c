60cbdc7f2f15f151a425dd9fa322673cfac2c4b5
Corbel99 <thomas.corbel35000@gmail.com>
2026-06-26 13:11:13 +0200

    Task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100 using FizzBuzz rules
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (i % 5 == 0)
+	{
+		if (i != 100)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("Buzz");
+		}
+	}
+	else
+	{
+		if (i != 100)
+		{
+			printf("%d ", i);
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+}
+return (0);
+}
