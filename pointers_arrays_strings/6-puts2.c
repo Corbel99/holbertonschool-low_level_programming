ac55fff2b647ce468311104b3042b74c6cbeaf5b
Corbel99 <thomas.corbel35000@gmail.com>
2026-06-30 17:59:22 +0200

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - Prints every other character of a string
+ * @str: Pointer to the string
+ */
+void puts2(char *str)
+{
+int n;
+
+for (n = 0; str[n] != '\0'; n = n + 2)
+{
+	_putchar(str[n]);
+}
+_putchar('\n');
+}
