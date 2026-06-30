e96e189f4946576dcdbb9bd8283b43b134fce930
Corbel99 <thomas.corbel35000@gmail.com>
2026-06-30 18:02:22 +0200

    task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,9 @@ void puts2(char *str)
 {
 int n;
 
-for (n = 0;str[n] != '\0'; n = n + 2)
+for (n = 0; str[n] != '\0'; n = n + 2)
 {
-	_putchar(str[n]);
+	_putchar("str[n]");
 }
-_putchar('\n');
+
 }
