be0b794ce4fb5b16e310347f551d6af968b25041
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-01 11:25:03 +0200

    task 2.2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncpy - Copies a string using at most n characters
  * @dest: Destination string
@@ -15,5 +16,12 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
 	return (dest);
 }
