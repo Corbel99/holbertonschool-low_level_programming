ea5f5732ec9fe9d452d25f9329d90f182a1de502
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-02 22:59:31 +0200

    task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: Pointer to the string to convert
+ *
+ * Return: The converted integer
+ */
+int _atoi(char *s)
+{
+	int i;
+	int result;
+	int sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign = -sign;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				result = result * 10 + (s[i] - '0');
+				i++;
+			}
+			return (result * sign);
+		}
+		i++;
+	}
+
+	return (0);
+}
