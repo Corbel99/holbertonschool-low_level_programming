190413ac8c701fbe37d61e0ba41441034ec31fe3
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-02 11:46:23 +0200

    Task 2.3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,5 +15,7 @@ int add(int, int);
 void print_to_98(int n);
 void print_rev(char *s);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
