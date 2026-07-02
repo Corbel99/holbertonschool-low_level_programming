694780d3e63fec87a5456ff1e14b771629fb3995
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-02 14:12:31 +0200

    Task 4.3

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: String to search
+ * @accept: String containing accepted characters
+ *
+ * Return: Pointer to the first matching character, or NULL if not found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (NULL);
+}
