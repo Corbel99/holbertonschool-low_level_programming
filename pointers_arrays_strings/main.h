75d88c24b772ce5afaf3b457994d8e9bc8312340
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-02 11:00:10 +0200

    Task 0.3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,9 +2,18 @@
 #define MAIN_H
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_rev(char *s);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
