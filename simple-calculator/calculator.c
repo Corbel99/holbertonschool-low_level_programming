9dc5f66a5ddde7377fa444ada9cb4197217ff25f
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-03 16:51:50 +0200

    calculator

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the Simple Calculator program.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int choice = -1;
+	int num1 = 0;
+	int num2 = 0;
+	int result = 0;
 
 	printf("Simple Calculator\n");
 
@@ -12,8 +20,37 @@ int main(void)
 		printf("Choice: ");
 		scanf("%d", &choice);
 
-		if (choice != 0)
+		if (choice >= 1 && choice <= 4)
+		{
+			printf("Enter first number: ");
+			scanf("%d", &num1);
+
+			printf("Enter second number: ");
+			scanf("%d", &num2);
+
+			if (choice == 1)
+				result = num1 + num2;
+			else if (choice == 2)
+				result = num1 - num2;
+			else if (choice == 3)
+				result = num1 * num2;
+			else if (choice == 4)
+			{
+				if (num2 != 0)
+					result = num1 / num2;
+				else
+				{
+					printf("Error: division by zero.\n");
+					continue;
+				}
+			}
+
+			printf("Result: %d\n", result);
+		}
+		else if (choice != 0)
+		{
 			printf("Invalid choice.\n");
+		}
 	}
 
 	printf("Bye!\n");
