9867da72e0944687ff2b63d38f606bd5a2fcf113
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-07 10:37:51 +0200

    task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - Creates a 2D array of integers
+ * @width: Number of columns
+ * @height: Number of rows
+ *
+ * Return: Pointer to the allocated 2D array, or NULL if it fails
+ */
+int **alloc_grid(int width, int height)
+{
+int i;
+int j;
+int **grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+
+		if (grid[i] == NULL)
+		{
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
