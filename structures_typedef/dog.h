acc04c16ffc8db4fb382d88132015d439d8f938f
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-08 15:01:53 +0200

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - stores information about a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
