93102cc78d179cbc5a7656df7a6550bb8c02d74a
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-09 15:33:14 +0200

    task 4

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,24 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /**
  * struct dog - stores information about a dog
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 
 typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
