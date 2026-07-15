860b5c19e8290a7155d97741a1f7a3f756267d3e
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-15 20:57:09 +0200

    task1

---
+++ function_pointers/1-array_itterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: pointer to the array
+ * @size: number of elements in the array
+ * @action: function to execute on each element
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		(*action)(array[i]);
+		i++;
+	}
+}
