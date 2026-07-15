e906268eda900319be64e8c08286ca0794cc6ce5
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-15 11:41:09 +0200

    task3.1

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,11 +10,19 @@
 
 int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
-	
-	(void)argc;
-	
+	int a;
+	int b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
 	printf("%d\n", a * b);
 
 	return (0);
