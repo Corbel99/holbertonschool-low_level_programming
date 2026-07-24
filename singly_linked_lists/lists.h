f657f772ba93dad43d7f9518e0597b3dba093ba2
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-24 10:54:50 +0200

    refactor: remove task comments from lists.h for cleaner code

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -16,16 +16,13 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* Task 0 */
 size_t print_list(const list_t *h);
 
-/* Task 1 */
 size_t list_len(const list_t *h);
 
-/* Task 2 */
 list_t *add_node(list_t **head, const char *str);
 
-/* Utility function */
+
 unsigned int _strlen(const char *str);
 
 list_t *add_node_end(list_t **head, const char *str);
