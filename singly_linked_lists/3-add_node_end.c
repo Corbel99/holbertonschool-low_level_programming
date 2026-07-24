89f897f0a6c778d09a5c95f1a18d25ddbb3097f3
Corbel99 <thomas.corbel35000@gmail.com>
2026-07-24 11:00:27 +0200

    task3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,6 +13,11 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
 	list_t *current;
+	unsigned int len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -25,7 +30,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->len = strlen(str);
+	new->len = len;
 	new->next = NULL;
 
 	if (*head == NULL)
@@ -37,9 +42,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	current = *head;
 
 	while (current->next != NULL)
-	{
 		current = current->next;
-	}
 
 	current->next = new;
 
